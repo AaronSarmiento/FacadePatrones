ad86058e4b64214a3abef46c6eed6d716e11ad78
carlarojas09 <128859296+carlarojas09@users.noreply.github.com>
2023-03-29 21:03:00 -0500

    Update Secciones.java
    Primer commit

--- src/facadepatrones/Secciones.java
+++ src/facadepatrones/Secciones.java
@@ -48,7 +48,75 @@ public class Secciones {
         String nombreArchivo = "farandula.html";
       String rutaArchivo = "D:\\Users\\santi\\Downloads\\FacadePatrones";
       FileWriter archivo = new FileWriter(rutaArchivo + nombreArchivo);
-      archivo.write("<html><head><title>Seccion Farandula</title></head><body><h1>Farandula</h1></body></html>");
+      archivo.write("<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Revista de Música</title>
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+			font-family: Arial, sans-serif;
+			background-color: #f5f5f5;
+		}
+		header {
+			background-color: #333;
+			color: #fff;
+			padding: 20px;
+			text-align: center;
+		}
+		h1 {
+			margin-top: 0;
+			font-size: 48px;
+		}
+		.container {
+			max-width: 800px;
+			margin: 0 auto;
+			padding: 20px;
+		}
+		.article {
+			margin-bottom: 30px;
+		}
+		.article h2 {
+			margin-top: 0;
+			font-size: 36px;
+			color: #333;
+		}
+		.article p {
+			font-size: 18px;
+			line-height: 1.5;
+			color: #666;
+		}
+		.article img {
+			max-width: 100%;
+			height: auto;
+			margin-bottom: 20px;
+		}
+		.article .meta {
+			font-size: 14px;
+			color: #999;
+			margin-top: 20px;
+		}
+	</style>
+</head>
+<body>
+	<header>
+		<h1>Farandula</h1>
+	</header>
+	<div class="container">
+		<div class="article">
+			<h2>Sebastian Lletget, prometido de Becky G, reconoció que engañó a la artista y se disculpó públicamente</h2>
+			<p>La vida amorosa de Becky G se volvió tema de conversación después de que surgieron rumores de que Sebastian Lletget, su prometido, la engañó con una joven en una discoteca de Madrid, España.</p>
+			<img src="https://e.rpp-noticias.io/normal/2023/03/24/034803_1406169.jpg" alt="Foto">
+			<p>El novio de Becky G rompió el silencio y reconoció públicamente que sí engañó a la artista. El hombre compartió un comunicado en sus redes sociales en el que aprovechó para disculparse con la intérprete de 'Sin pijama'.</p>
+			<p>"Has sido una luz en mi vida, mi fuerza, quien siempre me ha mostrado amor incondicional. En vez de honrar ese amor cada día, he hecho lo contrario, te he hecho daño e irrespetado a la persona que más amo. Lo siento y sé que tengo que hacer todo y más para ganarme tu confianza y el amor que mereces". Dijo el deportista</p>
+            <img src="https://intn24.lalr.co/old/Becky%20G%20y%20su%20novio%2C%20Sebastian%20Lletget.jpg?w=480" alt="Foto">
+			<div class="meta">Publicado el 27 de Marzo de 2023</div>
+		</div>
+	</div>
+</body>
+</html>");
       archivo.close();
       
       try {
